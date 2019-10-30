f1a2126ee6a9fe5399b4b55f7130a1deaa177cac
Fardhan Ardhi <fa.dsg.id@gmail.com>
2019-10-30 11:37:34 +0700

    fix tugas

---
+++ Tugas1/app/src/main/java/adan/polinema/ac/id/tugas1/models/SuperHero.java
@@ -0,0 +1,37 @@
+package adan.polinema.ac.id.tugas1.models;
+
+public class SuperHero {
+    int Gambar;
+    String heroName;
+    String desc;
+
+    public SuperHero(int gambar, String heroName, String desc) {
+        Gambar = gambar;
+        this.heroName = heroName;
+        this.desc = desc;
+    }
+
+    public int getGambar() {
+        return Gambar;
+    }
+
+    public void setGambar(int gambar) {
+        Gambar = gambar;
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
--- Tugas2/app/src/main/java/adan/polinema/ac/id/tugas2/MainActivity.java
+++ Tugas2/app/src/main/java/adan/polinema/ac/id/tugas2/MainActivity.java
@@ -1,14 +1,45 @@
 package adan.polinema.ac.id.tugas2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import adan.polinema.ac.id.tugas2.adapters.SuperHeroAdapter;
+import adan.polinema.ac.id.tugas2.models.SuperHero;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView rvSuperHero;
+    List<SuperHero> listSuperHero = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvSuperHero = findViewById(R.id.rvSuperHero);
+
+        SuperHero hero = new SuperHero(R.drawable.batman,"Batman","Identitas asli Batman adalah Bruce Wayne, seorang pengusaha yang kaya raya. Nama Bruce Wayne itu sendiri diambil dari nama tokoh sejarah, yaitu Robert the Bruce dan Anthony Wayne.");
+        listSuperHero.add(hero);
+
+        hero = new SuperHero(R.drawable.gareng,"Gareng","adalah punakawan yang berkaki pincang. Hal ini merupakan sebuah sanepa dari sifat Gareng sebagai kawula yang selalu hati-hati dalam bertindak. Selain itu, cacat fisik Gareng yang lain adalah tangan yang ciker atau patah.");
+        listSuperHero.add(hero);
+
+        hero = new SuperHero(R.drawable.superman,"Superman", "Superman adalah karakter superhero ciptaan Jerry Siegel (penulis) dan Joe Shuster (artist), yang kini menjadi ikon dunia yang hak ciptanya dipegang oleh DC Comic.");
+        listSuperHero.add(hero);
+
+        hero = new SuperHero(R.drawable.petruk,"Petruk", "adalah tokoh punakawan dalam pewayangan Jawa, di pihak keturunan/trah Witaradya. Petruk tidak disebutkan dalam kitab Mahabarata dari India.");
+        listSuperHero.add(hero);
+
+        SuperHeroAdapter superHeroAdapter = new SuperHeroAdapter(listSuperHero);
+
+        rvSuperHero.setAdapter(superHeroAdapter);
+        rvSuperHero.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+
+
     }
 }
